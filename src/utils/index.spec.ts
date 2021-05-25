187f9c0ab45841494ea9a757484d5d6fdd5fa0cd
ooanishoo <anish.maharjan12341@gmail.com>
2021-05-25 11:36:37 +1000

    Add utils and write unit tests

---
+++ src/utils/index.spec.ts
@@ -0,0 +1,52 @@
+import { getEntityFields, isValidSearchQuery } from '.'
+
+describe('getEntityFields()', () => {
+  test.each`
+    entityType         | searchableFields
+    ${`users`}         | ${['_id', 'url', 'external_id', 'email', 'name', 'alias', 'created_at', 'active', 'verified', 'shared', 'locale', 'timezone', 'last_login_at', 'phone', 'signature', 'organization_id', 'tags', 'suspended', 'role']}
+    ${`organizations`} | ${['_id', 'url', 'external_id', 'name', 'domain_names', 'created_at', 'details', 'shared_tickets', 'tags']}
+    ${`tickets`}       | ${['_id', 'url', 'external_id', 'created_at', 'type', 'subject', 'description?', 'priority', 'status', 'submitter_id', 'assignee_id', 'tags', 'has_incidents', 'due_at', 'via', 'organization_id']}
+  `(
+    `should return correct searchable fields when entity = $entityType`,
+    ({ entityType, searchableFields }) => {
+      const fields = getEntityFields(entityType)
+      expect(fields).toEqual(searchableFields)
+    }
+  )
+
+  test.each`
+    entityType
+    ${`people`}
+    ${`company`}
+    ${`tick`}
+  `(
+    `should throw error if entityType is invalid, entity = $entityType`,
+    ({ entityType }) => {
+      expect(() => getEntityFields(entityType)).toThrowError()
+    }
+  )
+})
+
+describe('isValidSearchQuery()', () => {
+  // hide console warn from tests
+  beforeEach(() => {
+    jest.spyOn(console, 'warn').mockImplementation(jest.fn())
+  })
+  test.each`
+    searchQuery                                      | expectedOutput
+    ${{ entity: 'users', field: '_id', value: 123 }} | ${true}
+    ${{ entity: 'users', field: '_id', value: 123 }} | ${true}
+    ${{ entity: 'users', field: '_id', value: 123 }} | ${true}
+    ${{ entity: 'users', field: '_id' }}             | ${true}
+    ${{ field: '_id', value: 123 }}                  | ${false}
+    ${{ entity: 'users', value: 123 }}               | ${false}
+    ${{ entity: 'users', value: 123 }}               | ${false}
+    ${{}}                                            | ${false}
+  `(
+    `should check if the search query is valid`,
+    ({ searchQuery, expectedOutput }) => {
+      const output = isValidSearchQuery(searchQuery)
+      expect(expectedOutput).toEqual(output)
+    }
+  )
+})
---
+++ src/utils/index.ts
@@ -0,0 +1,89 @@
+import path from 'path'
+import { EntityType, SearchQuery } from '../types'
+
+export const getFilePath = (fileName: string): string => {
+  const dirArray = __dirname.split('/')
+  const location = dirArray
+    .splice(0, dirArray.indexOf('search-engine') + 1)
+    .join('/')
+  const PATH = path.join(location, `./data/${fileName}.json`)
+  return path.resolve(PATH)
+}
+
+export const getEntityFields = (entityType: EntityType): string[] => {
+  switch (entityType) {
+    case 'users':
+      return [
+        '_id',
+        'url',
+        'external_id',
+        'email',
+        'name',
+        'alias',
+        'created_at',
+        'active',
+        'verified',
+        'shared',
+        'locale',
+        'timezone',
+        'last_login_at',
+        'phone',
+        'signature',
+        'organization_id',
+        'tags',
+        'suspended',
+        'role'
+      ]
+
+    case 'organizations':
+      return [
+        '_id',
+        'url',
+        'external_id',
+        'name',
+        'domain_names',
+        'created_at',
+        'details',
+        'shared_tickets',
+        'tags'
+      ]
+
+    case 'tickets':
+      return [
+        '_id',
+        'url',
+        'external_id',
+        'created_at',
+        'type',
+        'subject',
+        'description?',
+        'priority',
+        'status',
+        'submitter_id',
+        'assignee_id',
+        'tags',
+        'has_incidents',
+        'due_at',
+        'via',
+        'organization_id'
+      ]
+    default:
+      throw new Error(`Invalid Entity: ${entityType}`)
+  }
+}
+
+export const isValidSearchQuery = (searchQuery: SearchQuery): boolean => {
+  if (!searchQuery.entity) {
+    console.warn('Please select a valid entity')
+    return false
+  }
+  if (!searchQuery.field || searchQuery.field.length === 0) {
+    console.warn('Please select a valid searchable field')
+    return false
+  }
+  if (!searchQuery.value) {
+    console.warn('No search keyword supplied, searching for empty values')
+  }
+
+  return true
+}
